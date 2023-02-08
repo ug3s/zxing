3faae59bc740a58d108062ca0299b77949674b41
ug3s <ug3s@github.com>
2023-02-08 19:37:38 +0100

    Reduce to core only and add java module file to core lib

---
+++ core/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.google.zxing {
+	exports com.google.zxing.datamatrix.encoder;
+	exports com.google.zxing.qrcode;
+	exports com.google.zxing;
+	exports com.google.zxing.qrcode.decoder;
+	exports com.google.zxing.common;
+}
